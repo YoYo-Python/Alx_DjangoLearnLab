07405a808b4297abaa6555a82988d08de968b4b9
Youssef Waleed <yoyo.kotlin@gmail.com>
2025-03-21 02:19:33 +0200

    relationship part 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.http import HttpResponse
-from .models import Book, Library
+from .models import Library
+from .models import Book
 # Create your views here.
 def book_view(request):
     books = Book.objects.all()
